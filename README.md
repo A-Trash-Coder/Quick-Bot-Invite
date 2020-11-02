1d28eb27cb3870c29f6982429bc114b448f3c1fb
A-Trash-Coder <gavynlamar@gmail.com>
2020-11-02 16:20:26 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Quick-Bot-Add
+
+Adds a button to the User Context Menu for bots. This opens a link to invite the bot.
+
+![previe](https://i.adiscorduser.com/ZAkKFcf.png)
---
+++ index.js
@@ -0,0 +1,30 @@
+const { Plugin } = require('powercord/entities');
+const { getModule, React } = require('powercord/webpack');
+const { inject, uninject } = require('powercord/injector');
+
+module.exports = class QuickBotInvite extends Plugin {
+  async startPlugin() {
+    const menu = await getModule(["MenuItem"]);
+    const GuildChannelUserContextMenu = await getModule(m => m.default && m.default.displayName === 'GuildChannelUserContextMenu');
+    inject('quick-bot-invite', GuildChannelUserContextMenu, 'default', (args, res) => {
+      console.log(res)
+      if (args[0]['user']['bot'] && res.props) {
+        res.props.children.props.children.push(
+          React.createElement(menu.MenuItem, {
+            name: "Invite Bot",
+            separate: true,
+            id: "QuickBotInviteButton",
+            label: "Invite Bot",
+            action: () => window.open(`https://discord.com/oauth2/authorize?client_id=${args[0]['user']['id']}&scope=bot`),
+          })
+        );
+      }
+      return res
+    });
+
+  }
+
+  pluginWillUnload() {
+    uninject('quick-bot-invite')
+  }
+}
