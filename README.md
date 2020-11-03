cb2383a4c18163905650c0c55905470ba3fac8b0
A-Trash-Coder <gavynlamar@gmail.com>
2020-11-02 19:31:58 -0500

    Add ability to invite with current guilds permissions, or not

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Adds a button to the User Context Menu for bots. This opens a link to invite the bot.
 
-![previe](https://i.adiscorduser.com/ZAkKFcf.png)
+![preview](https://i.adiscorduser.com/sQ6cXE1.gif)
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { inject, uninject } = require('powercord/injector');
 
 module.exports = class QuickBotInvite extends Plugin {
   async startPlugin() {
+    await this.doImport()
     const menu = await getModule(["MenuItem"]);
     const GuildChannelUserContextMenu = await getModule(m => m.default && m.default.displayName === 'GuildChannelUserContextMenu');
     const oldDefault = GuildChannelUserContextMenu.default;
@@ -11,17 +12,62 @@ module.exports = class QuickBotInvite extends Plugin {
       if (args[0]['user']['bot'] && res.props) {
         res.props.children.props.children.push(
           React.createElement(menu.MenuItem, {
-            name: "Invite Bot",
-            separate: true,
             id: "QuickBotInviteButton",
             label: "Invite Bot",
-            action: () => window.open(`https://discord.com/oauth2/authorize?client_id=${args[0]['user']['id']}&scope=bot`),
+          }, React.createElement(menu.MenuItem, {
+            id: "no-perms",
+            label: "No Permissions",
+            action: () => window.open(`https://discord.com/oauth2/authorize?client_id=${args[0]['user']['id']}&scope=bot`)
+          }), React.createElement(menu.MenuItem, {
+            id: "current-guild-perms",
+            label: "Current Guild Permissions",
+            action: async () => window.open(`https://discord.com/oauth2/authorize?client_id=${args[0]['user']['id']}&scope=bot&permissions=${await this.getPermissionsRaw(args[0]['guildId'], args[0]['user']['id'])}`)
           })
-        );
-      }
+          ));
+      };
       return res
     });
-  Object.assign(GuildChannelUserContextMenu.default, oldDefault)
+    Object.assign(GuildChannelUserContextMenu.default, oldDefault)
+
+  }
+
+  async getPermissionsRaw(guildId, userId) {
+    let permissions = 0;
+
+    const guild = this.getGuild(guildId);
+    const member = this.getMember(guildId, userId);
+
+
+    if (guild && member) {
+      if (guild.ownerId === userId) {
+        permissions = Permissions.ADMINISTRATOR;
+      } else {
+        permissions |= guild.roles[guild.id].permissions;
+
+        for (const roleId of member.roles) {
+          permissions |= guild.roles[roleId].permissions;
+        }
+      }
+
+      if ((permissions & Permissions.ADMINISTRATOR) === Permissions.ADMINISTRATOR) {
+        return Object.values(Permissions).reduce((a, b) => a | b, 0);
+      }
+    }
+
+    return permissions;
+  }
+
+  async import(filter, functionName = filter) {
+    if (typeof filter === 'string') {
+      filter = [filter];
+    }
+
+    this[functionName] = (await getModule(filter))[functionName];
+  }
+
+  async doImport() {
+    await this.import('getMember');
+    await this.import('getGuild');
   }
 
   pluginWillUnload() {
